d2a2d121e0b5673c1a872f07210a211aa71d16d3
Gregory ruoss <gregory_ruoss@sluz.ch>
2025-04-04 10:51:11 +0200

    zusatz aufgabe mehr tests hinzugefügt tests schlagen auf fehler

--- src/darts.test.ts
+++ src/darts.test.ts
@@ -5,3 +5,15 @@ describe('calcPoints', () => {
     expect(calcPoints('3 20 1 17 2 4')).toBe(85);
   });
 });
+
+test('berechnet 2x15, 1x18, 3x19 korrekt', () => {
+    expect(calcPoints('2 15 1 18 3 19')).toBe(105);
+  });
+  
+  test('berechnet 3x20, 1x5 (1x daneben)', () => {
+    expect(calcPoints('3 20 1 5')).toBe(65);
+  });
+  
+  test('leerer Wurf ergibt 0 Punkte', () => {
+    expect(calcPoints('')).toBe(0);
+  });
