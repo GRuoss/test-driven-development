dd5372917f7837d2305d4df621f77d7cdd11b6c1
Gregory ruoss <gregory_ruoss@sluz.ch>
2025-04-04 10:59:06 +0200

    Zusatz aufgabe habe possiblechreckout test hinzugefügt sind flasch da funktion nochnicht besteht

--- src/darts.test.ts
+++ src/darts.test.ts
@@ -17,3 +17,17 @@ test('berechnet 2x15, 1x18, 3x19 korrekt', () => {
   test('leerer Wurf ergibt 0 Punkte', () => {
     expect(calcPoints('')).toBe(0);
   });
+  
+  describe('possibleCheckout', () => {
+    test('Double 12 bei 477', () => {
+      expect(possibleCheckout(477)).toBe('Double 12');
+    });
+  
+    test('Kein Checkout bei ungerader Zahl', () => {
+      expect(possibleCheckout(480)).toBeNull();
+    });
+  
+    test('Kein Checkout bei zu hohem Double', () => {
+      expect(possibleCheckout(441)).toBeNull();
+    });
+  });
